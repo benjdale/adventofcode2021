a504427989e707c28be979f700414b363faebf95
benjdale <60547871+benjdale@users.noreply.github.com>
2021-12-14 13:36:16 +0000

    Day 1 Part 1

--- day1/day1p1.py
+++ day1/day1p1.py
@@ -3,8 +3,7 @@ depth_increases = 0
 with open("../data/day1.txt") as file:
     lines = [int(line.strip()) for line in file]
     for index, line in enumerate(lines):
-        previous_line = lines[index - 1]
-        if line > previous_line:
+        if line > lines[index - 1]:
             depth_increases += 1
 
 print(f"{depth_increases} depth increases")
